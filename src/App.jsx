96f9c83bd005703ba35da6495c35fecb21d91fa9
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-12-03 14:34:05 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from './components/button/Button';
+// {} -> <>
+const App = () => {
+	return (
+		<>
+			<Button text='Click 1' name='Fer' />
+			<Button text='Click 2' name='Fer' />
+			<Button text='Click 3' name='Fer' />
+		</>
+	);
+};
+
+export default App;
